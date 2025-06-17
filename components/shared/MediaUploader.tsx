852430ab820c277f84e08dc7f9b7fa05416facce
ANVSLD SRIHARSHITHA <23b01a1207@svecw.edu.in>
2025-06-17 20:01:07 +0530

    Update MediaUploader.tsx

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -195,7 +195,7 @@ const MediaUploader = ({
                 className="media-uploader_cldImage"
                 crop="pad"
                 gravity="auto"
-                effects={["gen_fill"]}
+                effect="gen_fill"
               />
             </div>
           ) : (
